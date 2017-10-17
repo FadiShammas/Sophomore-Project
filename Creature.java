0dd59a75e30786e9c0b1ee13f1a8ad3c94c90a76
jmarginet <32486278+jmarginet@users.noreply.github.com>
2017-10-17 00:21:36 -0400

    Add files via upload

---
+++ Creature.java
@@ -0,0 +1,69 @@
+package lang.wars;
+
+/**
+ * Template for a creature card
+ * 
+ * @author Jake
+ */
+public class Creature extends Card {
+    
+    protected int hp, atk;
+
+    public Creature() {
+    }
+
+    public Creature(String name, String art, String desc, int cost, int hp, int atk) {
+        super(name, art, desc, cost);
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArt() {
+        return art;
+    }
+
+    public void setArt(String art) {
+        this.art = art;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+    
+    
+    
+}
